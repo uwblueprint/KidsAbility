e7c12e7f33901313f745bb88ffba3dbf342347b7
GodGreg <gmaxin@uwaterloo.ca>
2019-01-18 18:03:39 -0500

    Base Code/Router/HomePage/NoutFoundPage/Scripts/FireBase

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+//This imports the styling into the react app 
+//All styling in this file should be available to all components as this is the root page
 import './App.css';
 
-class App extends Component {
+//Imports
+
+//This is needed for every component
+import React, { Component } from 'react';
+
+//These are all npm packages 
+import {Router, Route, Switch, Redirect} from 'react-router';
+import {NotificationContainer} from 'react-notifications';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+//These are page components we are importing from our /src/pages folder
+import Home from '../src/pages/Home/Home'
+import NotFound from '../src/pages/NotFound/NotFound'
+
+//Create an instance of browserHistory
+const browserHistory = createBrowserHistory();
+
+export default class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <Router history={browserHistory}>
+                <div>
+                    <NotificationContainer/>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route component={NotFound}/>
+                    </Switch>
+                </div>
+            </Router>
         </header>
       </div>
     );
   }
 }
-
-export default App;
---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class NotFound extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h2>404 - Not Found</h2>
+                <p>Whoops! This page doesn't exist</p>
+            </div>
+        );
+    }
+}
