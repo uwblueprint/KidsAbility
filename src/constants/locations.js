e8edc23ae17f855c1ca33f4b061c5ae0318366df
Hannah Wang <hannahwang12@gmail.com>
2019-01-27 14:52:55 -0500

    first page of location data

---
+++ src/constants/locations.js
@@ -0,0 +1,91 @@
+const CITIES = {
+  Cambridge: 'Cambridge',
+  Guelph: 'Guelph',
+  Kitchener: 'Kitchener',
+  Waterloo: 'Waterloo',
+};
+
+const STATUSES = {
+  Active: 'Active',
+  Inactive: 'Inactive',
+}
+
+const LOCATIONS = {
+  'ALEXDC': {
+    description: 'W-Alexandra Day Care',
+    city: CITIES.Waterloo,
+    status: STATUSES.Active,
+  },
+  'ALLDAY': {
+    description: 'C-All In a Day\'s Play Day Care',
+    city: CITIES.Cambridge,
+    status: STATUSES.Active,
+  },
+  'APPLE': {
+    description: 'G-Appleseed Child Care Centre',
+    city: CITIES.Guelph,
+    status: STATUSES.Active,
+  },
+  'ARTHUR': {
+    description: 'G-Arthur School Readiness Prog',
+    city: CITIES.Guelph,
+    status: STATUSES.Active,
+  },
+  'ARTHURY': {
+    description: 'F-Arthur Y Childcare',
+    // not sure what the F means for city
+    status: STATUSES.Active,
+  },
+  'BRASON': {
+    description: 'C-Brason Academy Montessori',
+    city: CITIES.Cambridge,
+    status: STATUSES.Active,
+  },
+  'BRIGHTPATH': {
+    description: 'W-BrightPath Early Learning Centre',
+    city: CITIES.Waterloo,
+    status: STATUSES.Active,
+  },
+  'BRIGHTSTAR': {
+    description: 'W-Bright Starts Daycare',
+    city: CITIES.Waterloo,
+    status: STATUSES.Active,
+  },
+  'BUTTERFLY': {
+    description: 'W-Butterfly Learning Centre',
+    city: CITIES.Waterloo,
+    status: STATUSES.Active,
+  },
+  'C-CLIENT': {
+    description: 'C-Cambridge Client Home',
+    city: CITIES.Cambridge,
+    status: STATUSES.Active,
+  },
+  'C-ELITE': {
+    description: 'C-Elite Daycare',
+    city: CITIES.Cambridge,
+    status: STATUSES.Active,
+  },
+  'CAMBALLIGA': {
+    description: 'Camb Alligator',
+    city: CITIES.Cambridge,
+    status: STATUSES.Active,
+  },
+  'CAMBBEAR': {
+    description: 'Camb Bear',
+    city: CITIES.Cambridge,
+    status: STATUSES.Active,
+  },
+  'CAMBBOARD': {
+    description: 'Camb Board Room',
+    city: CITIES.Cambridge,
+    status: STATUSES.Active,
+  },
+  'CAMBCHEETA': {
+    description: 'Camb Cheetah',
+    city: CITIES.Cambridge,
+    status: STATUSES.Active,
+  },
+};
+
+export default LOCATIONS;
