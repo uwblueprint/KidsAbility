b09c8d72ee0706e363e014199a18f1a2a1ea6843
Hannah Wang <hannahwang12@gmail.com>
2019-02-10 18:41:49 -0500

    starter code

--- src/pages/View/View.js
+++ src/pages/View/View.js
@@ -1,15 +1,41 @@
 import React, {Component} from 'react';
+import ChartView from './ChartView/ChartView';
+import CalendarView from './CalendarView/CalendarView';
+import './View.css';
 
 export default class View extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            view: 'chart',
+        };
     }
 
-    render() {
+    toggleView = (e) => {
+        this.setState({ view: e.target.value });
+    }
+
+    render() {  
         return (
             <div className="container">
-                <h2>View page :)</h2>
+                <h2>Available Times</h2>
+                <button
+                    onClick={this.toggleView}
+                    data={this.props.data}
+                    disabled={this.state.view === 'chart'}
+                    value="chart"
+                >
+                    Chart View
+                </button>
+                <button
+                    onClick={this.toggleView}
+                    data={this.props.data}
+                    disabled={this.state.view === 'calendar'}
+                    value="calendar"
+                >
+                    Calendar View
+                </button>
+                {this.state.view === 'chart' ? <ChartView /> : <CalendarView />}
             </div>
         );
     }
