c5edb5d61de2922ac4ab036854bca3dce017de15
Cathy <cathy.wang@shopify.com>
2019-02-20 21:00:45 -0500

    use react select

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,19 +1,59 @@
 import React, {Component} from 'react';
-import form from 'react-bootstrap'
+import Select from 'react-select';
+
+const options = [
+  {value: 'chocolate', label: 'Chocolate'},
+  {value: 'strawberry', label: 'Strawberry'},
+  {value: 'vanilla', label: 'Vanilla'}
+]
 
 export default class Search extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedOption: null,
+      // add other input fields
+      // time: '',
+    };
+  }
 
-    render() {
-        return (
-            <div className="container">
-                <h2>Search Page</h2>
+  handleChange = (selectedOption) => {
+    this.setState({selectedOption})
+    console.log(`Option selected:`, selectedOption)
+    // set state based on input value
+  }
 
-                <form></form>
-            </div>
-        );
-    }
-}
+  // handleSubmit = (e) => {
+  //   // make request to backend/db based on form input (get the input from this.state)
+  // }
+  
+  render() {
+    const { selectedOption } = this.state;
+    return (
+      <Select
+        value={selectedOption}
+        onChange={this.handleChange}
+        options={options}
+      />
+      // <div className="search">
+      //   <h1>Find Available Times</h1>
+      //   <form className="searchForm" onSubmit={this.handleSubmit}>
+      //     <div className="formColumn1">
+      //       {/* components/elements for selecting clinician, service type, location */}
+
+      //     </div>
+      //     <div className="formColumn2">
+      //       {/* example 'input' element with onChange handler */}
+      //       <label>
+      //         Time
+      //         <input value={this.state.time} onChange={this.handleChange} />
+      //       </label>
+
+      //       <input type="submit" value="Search" />
+      //     </div>
+      //   </form>
+        
+      // </div>
+    );
+  }
+};
