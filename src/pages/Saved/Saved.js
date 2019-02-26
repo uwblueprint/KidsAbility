47dbd41a17f941904d403f3f7bf4d23d7fcf88b4
Cathy <cathy.wang@shopify.com>
2019-02-25 20:15:57 -0500

    initial commit for saved times page

--- src/pages/Saved/Saved.js
+++ src/pages/Saved/Saved.js
@@ -1,4 +1,8 @@
 import React, {Component} from 'react';
+import moment from 'moment';
+import ScrollArea from 'react-scrollbar'
+import { data } from './data';
+import './Saved.css';
 
 export default class Saved extends Component {
     constructor(props) {
@@ -7,10 +11,44 @@ export default class Saved extends Component {
     }
 
     render() {
+
+        console.log(data);
+
         return (
-            <div className="container">
-                <h2>Saved Time page :)</h2>
+            <div>
+                <h1>Saved Times</h1>
+                <ScrollArea
+                    speed={0.8}
+                    className="area"
+                    contentClassName="content"
+                    horizontal={false}
+                    style={{ height: "300px" }}
+                >
+                    <div >                
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Date</th>
+                                    <th>Time</th>
+                                    <th>Location</th>
+                                    <th>Save</th>
+                                </tr>
+                            </thead>
+                        
+                            {
+                                data.map((elem) =>
+                                <tr>
+                                    <td>{elem.Date}</td>
+                                    <td>{elem.Start} - {elem.End}</td>
+                                    <td>{elem.Location.charAt(0) + elem.Location.substr(1).toLowerCase()}</td>
+                                    <td>{elem.saved}</td>
+                                </tr>)
+                            }
+                            
+                        </table>
+                    </div>
+                </ScrollArea>
             </div>
-        );
+        )
     }
 }
