0f412f324d3c1844cb90edefb5095d800786252b
GodGreg <gmaxin@uwaterloo.ca>
2019-03-04 19:19:45 -0500

    Initial

---
+++ src/constants/programs.js
@@ -0,0 +1,18 @@
+const STATUSES = {
+  Active: 'Active',
+  Inactive: 'Inactive',
+}
+
+const GROUPS = {
+    AB: 'AB',
+    Special: 'Special',
+    AS: 'AS',
+}
+
+const PROGRAMS = {
+    'ABA-ABA': {
+        description: "",
+        groups: [GROUPS.AB, GROUPS.Special, GROUPS.AS],
+        status: "",
+    }
+}
