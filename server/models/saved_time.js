2cedf839ba95262f72e3a67e35b317a252015fd3
GodGreg <gmaxin@uwaterloo.ca>
2019-03-13 11:44:14 -0400

    fixed saved-time schema

--- server/models/saved_time.js
+++ server/models/saved_time.js
@@ -2,85 +2,55 @@ var mongoose = require('mongoose');
 	
 	
 var savedSchema = new mongoose.Schema({	
-	names: {
-		type: [[name]]
+	Date: {
+		type: String
 	},
-	services: { 
-		type: [[service]]
+	Start: {
+		type: String
 	},
-	location: {
-		type: {locationSchema}
+	End: {
+		type: String
+	}, 
+	Duration: {
+		type: Number
 	},
-	time: {
-		type: {time}
+	LastName: {
+		type: String
 	},
-	timeOfDay: {
-		type: {timeOfDay}
+	FirstName: {
+		type: String
 	},
-	numSessions: {
-		type: {numSession}
+	ID:{
+		type: Number
+	},
+	Email: {
+		type: String
+	},
+	ClientLast: {
+		type: String
+	},
+	ClientFirst: {
+		type: String
+	},
+	ClientID: {
+		type: String
+	},
+	Location: {
+		type: String
+	},
+	Service: {
+		type: String
+	},
+	Treatment: {
+		type: String
+	},
+	Program: {
+		type: String
+	},
+	ServiceDescription: {
+		type: String
 	}
 }, { collection: 'SavedTimes' });
-
-var name = new mongoose.Schema({
-	first: {
-		type: String
-	},
-	last: {
-		type: String
-	},
-	label: {
-		type: String
-	},
-	value: {
-		type: String
-	}
-});
-
-var service = new mongoose.Schema({
-	label: {
-		type: String
-	},
-	value: {
-		type: String
-	}
-});
-
-var locationSchema = new mongoose.Schema({
-	label: {
-		type: String
-	},
-	value: {
-		type: String
-	}
-});
-
-var numSession = new mongoose.Schema({
-	label: {
-		type: Number
-	},
-	value: {
-		type: String
-	}
-});
-
-var time = new mongoose.Schema({
-	label: {
-		type: Number
-	},
-	value: {
-		type: String
-	}
-});
-
-var timeOfDay = new mongoose.Schema({
-	label: {
-		type: String
-	},
-	value: {
-		type: String
-	}
-});
 
 	
 var Saved = mongoose.model('Saved', savedSchema);
