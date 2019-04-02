b2114fb99fe7805b23e6927f9bccf45b6916ec42
Hannah Wang <hannahwang12@gmail.com>
2019-04-01 22:53:27 -0400

    find and display overlapping times

--- src/pages/View/ChartView/ChartView.js
+++ src/pages/View/ChartView/ChartView.js
@@ -14,8 +14,6 @@ export default class ChartView extends Component {
     }
 
     render() {
-        console.log(this.props.data);
-        console.log(this.props.clinicians);
         return (
             <div className="table">
             <table>
@@ -43,7 +41,10 @@ export default class ChartView extends Component {
                                         <td className="wide">{moment(elem.Start, 'h:mm').format('h:mm a')} - {moment(elem.End, 'h:mm').format('h:mm a')}</td>
                                         <td>{elem.Location.charAt(0) + elem.Location.substr(1).toLowerCase()}</td>
                                         <td>
-                                            <div className="dot" style={{ backgroundColor: this.props.clinicians[elem.Name].color }}></div>
+                                            <div className="dots">
+                                                {elem.Names.map((e, i) =>
+                                                    <div className="dot" style={{ backgroundColor: this.props.clinicians[e].color }} key={i}></div>)}
+                                            </div>
                                         </td>
                                         <td>
                                             <Icon className="save-button" style={{color: "purple"}} onClick={this.onClickSave}>
