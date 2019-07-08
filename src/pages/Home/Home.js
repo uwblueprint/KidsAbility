3b476a03ab8a9c276f6fd9ede85a532ea1000f24
KarolinaXin <karolinaxin@yahoo.ca>
2019-07-08 17:13:12 -0400

    WIP: Home Page UI

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,15 @@
 import React, {Component} from 'react';
 import {Router, Route, Switch, Redirect} from 'react-router';
 import './Home.css';
+import '../Cancel.svg';
+import { View, Text, Image } from 'react-native';
+import { Card, ListItem, Button, Icon } from 'react-native-elements';
 
+/*
+Plan: Use Card to have the image and text stored in it and make it clickable
+      Then use the Division for Row and Column to make three Cards for the three options
+      And in row above it, have the Hello, NAME text
+*/
 export default class Home extends Component {
 
     //This is a constructor
@@ -40,6 +48,18 @@ export default class Home extends Component {
                 <h2>Welcome
                     <b>{" " + this.state.user}</b>
                 </h2>
+                <Card> Karolina Fabulous </Card>
+                <Card> Karolina Fabulous </Card>
+                <Card> Karolina Fabulous </Card>
+                <Card>
+                <Card.Img variant="top" src="https://cdn1.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg" />
+                   <Card.Body>
+                   <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk
+                    of the card's content.
+                   </Card.Text>
+                   </Card.Body>
+                </Card>
                 <button className="button-find" onClick={this.availTimes}>Find Available Time</button>
                 <br></br>
                 <button className="button-save" onClick={this.savedTimes}>View Saved Times</button>
